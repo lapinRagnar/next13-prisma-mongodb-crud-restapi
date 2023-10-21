8f03ef1cf95eb07e17fc9daea4581f96a765cfb9
lapinRagnar <lapinragnar@gmail.com>
2023-10-22 00:52:11 +0200

    multiple api

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -8,14 +8,30 @@ const getPostsData = async () => {
 
   return res.json()
 }
+const getUsersData = async () => {
+    
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+
+  await new Promise(resolve => setTimeout(resolve, 2000)) // simulate a network latency
+
+  return res.json()
+}
 
 const Post = async () => {
 
-  const posts = await getPostsData()
+  // const posts = await getPostsData()
+  const [posts, users] = await Promise.all([getPostsData(), getUsersData()])
 
   return (
     <div>
       <h1 className="text-4xl mb-10">Post Page</h1>
+
+      <h2 className="text-2xl">Users</h2>
+      {
+        users.map(user => (
+          <p className="text-blue-800 " key={user.id}>{user.name}</p>
+        ))
+      }
 
       <ul className="flex flex-col gap-5">
         {
