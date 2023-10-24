83415d739f68c1b455f0c42b02f8cc1b802f7e70
lapinRagnar <lapinragnar@gmail.com>
2023-10-24 18:33:00 +0200

    le formulaire a'ajout

--- app/components/AddPost.jsx
+++ app/components/AddPost.jsx
@@ -7,6 +7,20 @@ import Modal from "./Modal"
 const AddPost = () => {
 
   const [modalOpen, setModalOpen] = useState(false)
+  const [inputs, setInputs] = useState({})
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("mes inputs", inputs)
+    setInputs({})
+  }
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setInputs(prevState => ({...prevState, [name]: value}) )
+  }
+
+
   return (
     <div>
       <button 
@@ -16,7 +30,32 @@ const AddPost = () => {
         Add New Post
       </button>
 
-      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>le modal est ici</Modal>
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        <form className="w-full" onSubmit={handleSubmit}>
+          <h1 className="text-2xl font-bold pb-3">Ajouter un nouveau post</h1>
+          <input
+            type="text"
+            placeholder="Titre du post"
+            name="title"
+            className="w-full p-2 mb-3"
+            value={inputs.title || ''} 
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="description du post"
+            name="description"
+            className="w-full p-2 my-5" 
+            value={inputs.description || ''} 
+            onChange={handleChange}
+          />
+          <button
+            type="submit"
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+            Valider
+          </button>
+        </form>
+      </Modal>
 
     </div>
   )
