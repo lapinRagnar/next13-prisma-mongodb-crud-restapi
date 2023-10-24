88f58c9b8efeb10515acb73e326fed7b5aefd23f
lapinRagnar <lapinragnar@gmail.com>
2023-10-24 17:10:06 +0200

    creer la composant AddPost

--- app/crud/page.jsx
+++ app/crud/page.jsx
@@ -1,8 +1,14 @@
+import AddPost from "../components/AddPost"
 
 
 const Crud = () => {
   return (
-    <div>Crud</div>
+    <div className="max-w-4xl mx-auto mt-4 ">
+      <div className="my-5 flex flex-col gap-4">
+        <h1 className="text-4xl font-bold">To do list</h1>
+        <AddPost />
+      </div>
+    </div>
   )
 }
 
